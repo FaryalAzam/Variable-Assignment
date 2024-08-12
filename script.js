de38d56f6001435c318f8dfbd4fa803fc4673489
FARYAL JAFFER <faryalazam212@gmail.com>
2024-08-13 01:22:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,2 @@
+document.write('<h2> Rule for naming JS variables </h2>');
+document.write('Variable name can only contain , number, $ and _ . For example : $my_1st Variable <br/> Variable must begin with a letter, $ or _ .For example : $name, _name or name <br/> Variable name are case sensitive <br/> Variable name should not be JS keywords')
